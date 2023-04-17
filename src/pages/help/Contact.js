eb7ece85223a487ec7cd74e39950886ec509e6fc
rakeshvngowda <rakeshvngowda@gmail.com>
2023-04-17 21:09:39 +0530

    part-4 changes

---
+++ src/pages/help/Contact.js
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h3>Contact Us</h3>
+      <form>
+        <label>
+          <span>Your Email:</span>
+          <input type="email" name="email" id="email" required />
+        </label>
+        <label>
+          <span>Your message</span>
+          <textarea name="message" required></textarea>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
